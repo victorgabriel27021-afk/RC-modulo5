07a3f5add8f99f98d05a0b4da9d78119ec90fe49
Victor Gabriel <victorgabriel27021@gmail.com>
2026-07-13 21:05:27 -0300

    inicio da resoluçao do problema das views

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,6 +1,14 @@
 Rails.application.config.assets.version = "1.0"
 
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "fonts")
+
 Rails.application.config.assets.precompile += %w[
+  *.svg
+  *.eot
+  *.woff
+  *.woff2
+  *.ttf
+
   jquery.min.js
   bootstrap.min.js
   metisMenu.min.js
