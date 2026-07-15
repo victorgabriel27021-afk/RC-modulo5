b356d190d0db57d96b6231f6f497d6f6d6e10a50
Victor Gabriel <victorgabriel27021@gmail.com>
2026-07-15 20:50:27 -0300

    2

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,4 @@
+//= require jquery.min
+//= require bootstrap.min
+//= require metisMenu.min
+//= require admins_backoffice/sb-admin-2
--- app/controllers/admins_backoffice/welcome_controller.rb
+++ app/controllers/admins_backoffice/welcome_controller.rb
@@ -1,6 +1,6 @@
 class AdminsBackoffice::WelcomeController < ApplicationController
   layout "admins_backoffice"
-  
+
   def index
   end
 end
