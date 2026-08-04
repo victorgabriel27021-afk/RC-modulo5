c00a756cd34e0a5c0237c036f6cda652439fe2a3
Victor Gabriel <victorgabriel27021@gmail.com>
2026-08-04 18:35:09 -0300

    Testes

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,6 +1,7 @@
 class ApplicationJob < ActiveJob::Base
   # Automatically retry jobs that encountered a deadlock
   # retry_on ActiveRecord::Deadlocked
+  # 4
 
   # Most jobs are safe to ignore if the underlying records are no longer available
   # discard_on ActiveJob::DeserializationError
