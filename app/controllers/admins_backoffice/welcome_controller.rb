6aad570ac8fb3b89cac657c2ac281543db3fc53b
Victor Gabriel <victorgabriel27021@gmail.com>
2026-08-11 19:02:46 -0300

    Rc - modulo 5 - admins routes

--- app/controllers/admins_backoffice/welcome_controller.rb
+++ app/controllers/admins_backoffice/welcome_controller.rb
@@ -1,8 +1,8 @@
 class AdminsBackoffice::WelcomeController < ApplicationController
+  before_action :authenticate_admin!
   layout "admins_backoffice"
 
   def index
-
-  @admins = Admin.all
+    @admins = Admin.all
   end
 end
