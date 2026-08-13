570339ed99aab2305ed625a45c0aebbd8c1e17ba
Victor Gabriel <victorgabriel27021@gmail.com>
2026-08-13 17:29:07 -0300

    migrates att - RC - M5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_06_17_154717) do
+ActiveRecord::Schema[7.2].define(version: 2026_08_13_202730) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -21,6 +21,14 @@ ActiveRecord::Schema[7.2].define(version: 2026_06_17_154717) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +42,6 @@ ActiveRecord::Schema[7.2].define(version: 2026_06_17_154717) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "questions", "users"
 end
